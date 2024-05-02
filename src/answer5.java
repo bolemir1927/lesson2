357e3bcaa8e09b1e6326a6c003b47e0356980a16
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-02 21:17:13 +0300

    0205

---
+++ src/answer5.java
@@ -0,0 +1,24 @@
+public class answer5 {
+    public static void main(String[] args) {
+        int number = 2;
+        switch (number){
+            case 1:
+                System.out.println("один");
+                break;
+            case 2:
+                System.out.println("два");
+                break;
+            case 3:
+                System.out.println("три");
+                break;
+            case 4:
+                System.out.println("четыре");
+                break;
+            case 5:
+                System.out.println("пять");
+                break;
+            default:
+                System.out.println("Закончили");
+        }
+    }
+}
