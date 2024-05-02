bd7b99cd45fdcbc5c65be4a794848b193aea70c1
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-02 21:04:42 +0300

    0205

--- src/answer4.java
+++ src/answer4.java
@@ -1,5 +1,17 @@
 public class answer4 {
     public static void main(String[] args) {
-
+        int a = 5;
+        int b = 7;
+        int op = 2;
+        if (op == 1)
+            System.out.println(a + b);
+        else if (op == 2 && a > b)
+            System.out.println(a - b);
+        else if (op == 3)
+            System.out.println(a * b);
+        else if (op == 4 && b > 0)
+            System.out.println(a / b);
+        else
+            System.out.println("eror");
     }
 }
